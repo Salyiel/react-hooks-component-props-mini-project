51af7b9f1b0998d1913387374045a76a86643e01
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-05-20 12:58:45 +0300

    Components created

---
+++ src/components/Article.js
@@ -0,0 +1,32 @@
+function emojiInMinutes(minutes){
+    let interval = 5;
+    let emoji = '☕️';
+    let emojis = ''
+  
+    if(minutes >= 30) {
+      interval = 10;
+    }
+  
+    for (let i = 0; i < minutes; i += interval){
+      emojis += emoji;
+    }
+  
+    return emojis;
+  }
+  
+  
+  
+  
+  function Article({title, date= "January 1, 1970", preview, minutes}) {
+    return (
+      <article>
+        <h3>{title}</h3>
+        <small>
+          {date} | {emojiInMinutes(minutes)} {minutes} mins read
+          </small>
+        <p>{preview}</p>
+      </article>
+    );
+  }
+  
+  export default Article;
